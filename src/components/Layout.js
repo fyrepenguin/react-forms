081d226ea259477ea197206bb3dec1f6e370928b
Mohit Kamal <mohit.kamal@grappus.com>
2022-07-22 08:17:23 +0530

    Changing the title from Todo to Todo List Application

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import AuthStatus from './AuthStatus';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import AuthStatus from "./AuthStatus";
 
 const Layout = () => {
   return (
-    <div className='main-container'>
+    <div className="main-container">
       <AuthStatus />
       <header className="app-title">
-        <h2>Todo </h2>
+        <h2>Todo List Application</h2>
       </header>
 
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
